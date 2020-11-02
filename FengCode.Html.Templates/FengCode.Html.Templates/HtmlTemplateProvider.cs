7500a3c0140481c186edac19990e10aeb3e64413
fengcode <fengcode@hotmail.com>
2020-11-02 17:08:29 +0800

    提交

---
+++ FengCode.Html.Templates/FengCode.Html.Templates/HtmlTemplateProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FengCode.Html.Templates
+{
+    /// <summary>
+    /// HTML模板提供器
+    /// </summary>
+    public static class HtmlTemplateProvider
+    {
+        /// <summary>
+        /// 服务器内部错误
+        /// </summary>
+        public static string Error500
+        {
+            get { return Properties.Resources.error_500; }
+        }
+
+        /// <summary>
+        /// 服务暂时不可用
+        /// </summary>
+        public static string Error503
+        {
+            get { return Properties.Resources.error_503; }
+        }
+
+        /// <summary>
+        /// 网站错误的默认模板
+        /// </summary>
+        public static string ErrorDefault
+        {
+            get { return Properties.Resources.error_default; }
+        }
+        /// <summary>
+        /// 404模板（简约风格）
+        /// </summary>
+        public static string Error404
+        {
+            get
+            {
+                return Properties.Resources.error_404;
+            }
+        }
+
+        /// <summary>
+        /// 404模板（紫色星空动画）
+        /// </summary>
+        public static string Error404_Star
+        {
+            get { return Properties.Resources.error_404_star; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # FengCode.Html.Templates
-HTML 模板
+HTML模板内容，包含404、500、503等模板。
+
+## 安装
+
+```
+PM> Install-Package FengCode.Html.Templates
+```
+
+## 使用
+
+公开类：`HtmlTemplateProvider`，调用该类的静态方法即可。
+
